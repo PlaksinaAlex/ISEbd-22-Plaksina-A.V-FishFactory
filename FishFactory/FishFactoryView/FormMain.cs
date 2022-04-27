63c0991a9440c48a52db0f147aef98e1efeae75f
Александра Плаксина <plaksinaaleksandra943@gmail.com>
2022-04-27 21:37:19 +0400

    lab4_hard

--- FishFactory/FishFactoryView/FormMain.cs
+++ FishFactory/FishFactoryView/FormMain.cs
@@ -133,5 +133,69 @@ namespace FishFactoryView
 		{
 			LoadData();
 		}
+		private void ComponentCannedToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var form = Program.Container.Resolve<FormReportCannedComponents>();
+			form.ShowDialog();
+		}
+
+		private void OrdersToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var form = Program.Container.Resolve<FormReportOrders>();
+			form.ShowDialog();
+		}
+
+		private void CannedsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			using var dialog = new SaveFileDialog
+			{
+				Filter = "docx|*.docx"
+			};
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				_reportLogic.SaveCannedsToWordFile(new ReportBindingModel
+				{
+					FileName = dialog.FileName
+				});
+				MessageBox.Show("Выполнено", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+		private void складыToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var form = Program.Container.Resolve<FormWareHouses>();
+			form.ShowDialog();
+		}
+
+		private void списокСкладовToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			using var dialog = new SaveFileDialog { Filter = "docx|*.docx" };
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				_reportLogic.SaveWareHousesToWordFile(new ReportBindingModel
+				{
+					FileName = dialog.FileName
+				});
+				MessageBox.Show("Выполнено", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+		private void списокЗаказовПоДатамToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var form = Program.Container.Resolve<FormReportOrdersByDate>();
+			form.ShowDialog();
+		}
+
+		private void складыСКомпонентамиToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var form = Program.Container.Resolve<FormReportWareHouseComponents>();
+			form.ShowDialog();
+		}
+
+		private void пополнениеСкладаToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var form = Program.Container.Resolve<FormWareHouseRestocking>();
+			form.ShowDialog();
+		}
 	}
 }
